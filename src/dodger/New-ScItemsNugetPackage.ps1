dceec5c26ec276226674dd48c899b26d0ce1ceb0
Yannis Guedel <yannis.guedel@unic.com>
2014-11-05 15:21:18 +0100

    implemented creating nuget package

---
+++ src/dodger/New-ScItemsNugetPackage.ps1
@@ -0,0 +1,21 @@
+function New-ScItemsNugetPackage
+{
+  [CmdletBinding()]
+  Param(
+      [Parameter(Mandatory=$true)]
+      [string] $ItemsFile,
+      [Parameter(Mandatory=$true)]
+      [string] $PackageName,
+      [Parameter(Mandatory=$true)]
+      [string] $Version,
+      [Parameter(Mandatory=$true)]
+      [string] $OutputFolder = ".",
+      [string] $NugetCommand = "nuget"
+  )
+  Process
+  {
+    $ItemsFile = Resolve-Path $ItemsFile
+    $OutputFolder = Resolve-Path $OutputFolder
+    & $nugetCommand "pack" "$PSScriptRoot\Items.Template.nuspec" -p "ID=$PackageName" -p "SourceFile=$ItemsFile" -version $Version -OutputDirectory $OutputFolder
+  }
+}
