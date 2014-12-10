f336129e4381427b2c59309d33b463b931d72e57
Yannis Guedel <yannis.guedel@unic.com>
2014-12-10 14:05:46 +0100

    implemented build pinned

--- src/dodger/Set-BuildPinned.ps1
+++ src/dodger/Set-BuildPinned.ps1
@@ -3,10 +3,22 @@ function Set-BuildPinned
   [CmdletBinding()]
   Param(
       [Parameter(Mandatory=$true)]
-      [string] $BuildId
+      [string] $BuildId,
+      [Parameter(Mandatory=$true)]
+      [string] $User,
+      [Parameter(Mandatory=$true)]
+      [string] $Password,
   )
   Process
   {
-    
+      $locator = "id:$BuildId"
+      $url = "https://teamcity.unic.com/httpAuth/app/rest/builds/$locator/pin"
+      $authInfo = $user + ":" + $password
+      $authInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::Default.GetBytes($authInfo))
+      $request = [System.Net.WebRequest]::Create($url)
+
+      $request.Headers["Authorization"] = "Basic " + $authInfo
+      $request.Method = "PUT"
+      $request.GetResponse()
   }
 }
