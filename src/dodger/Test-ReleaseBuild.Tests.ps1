348fd59b5fb38670299f8407cd9c831f9793c9c7
Yannis Guedel <yannis.guedel@unic.com>
2014-12-10 15:54:57 +0100

    added support for refs/heads

--- src/dodger/Test-ReleaseBuild.Tests.ps1
+++ src/dodger/Test-ReleaseBuild.Tests.ps1
@@ -3,22 +3,28 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 . "$here\$sut"
 
 Describe "Test-ReleaseBuild" {
-  Context "When providing release branch" {
+    Context "When providing release branch" {
 
     It "Should return true" {
        Test-ReleaseBuild "release/0.1"| Should Be $true
     }
-  }
-  Context "When providing hotfix branch" {
+    }
+    Context "When providing hotfix branch" {
 
       It "Should return true" {
         Test-ReleaseBuild "hotfix/0.1.1"| Should Be $true
       }
-  }
-  Context "When providing develop branch" {
+    }
+    Context "When providing hotfix branch" {
 
-      It "Should return true" {
-          Test-ReleaseBuild "develop"| Should Be $false
-      }
-  }
+        It "Should return true" {
+            Test-ReleaseBuild "refs/heads/hotfix/0.1.1"| Should Be $true
+        }
+    }
+    Context "When providing develop branch" {
+
+        It "Should return true" {
+            Test-ReleaseBuild "develop"| Should Be $false
+        }
+    }
 }
--- src/dodger/Test-ReleaseBuild.ps1
+++ src/dodger/Test-ReleaseBuild.ps1
@@ -1,13 +1,15 @@
 function Test-ReleaseBuild
 {
-  [CmdletBinding()]
-  Param(
-      [Parameter(Mandatory=$true)]
-      [string] $Branch
-  )
-  Process
-  {
-    $Branch -like "release/*" -or
-    $Branch -like "hotfix/*"
-  }
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true)]
+        [string] $Branch
+    )
+    Process
+    {
+        $Branch = $Branch -replace "refs/heads/"
+
+        $Branch -like "release/*" -or
+        $Branch -like "hotfix/*"
+    }
 }
